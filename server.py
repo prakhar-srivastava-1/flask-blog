e17d9229cd661888fcca32adc5682af5d8d4698b
Prakhar Srivastava <prakhar.srivastava.2409@gmail.com>
2022-01-30 02:52:21 +0530

    pick only the selected blog

--- server.py
+++ server.py
@@ -18,5 +18,24 @@ def index():
                            posts=posts)
 
 
+@app.route("/blog/<int:post_id>")
+def get_blog(post_id):
+    # get dummy blogs from npoint
+    url = "https://api.npoint.io/c790b4d5cab58020d391"
+    response = requests.request(url=url, method='GET')
+    posts = response.json()
+    select_post = dict()
+
+    for post in posts:
+        if post["id"] == post_id:
+            select_post = post
+            break
+
+    copyright_year = dt.datetime.now().strftime("%Y")
+    return render_template("blog.html",
+                           copyright_year=copyright_year,
+                           post=select_post)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
