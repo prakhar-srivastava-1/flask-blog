111b5e07c57a15ff32c4d69de0af1e2600a46f2c
Prakhar Srivastava <prakhar.srivastava.2409@gmail.com>
2022-01-30 02:30:58 +0530

    add api call to get dummy blogs from npoint

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import datetime as dt
 
+import requests
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -7,8 +8,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    # get dummy blogs from npoint
+    url = "https://api.npoint.io/c790b4d5cab58020d391"
+    response = requests.request(url=url, method='GET')
+    posts = response.json()
     copyright_year = dt.datetime.now().strftime("%Y")
-    return render_template("index.html", copyright_year=copyright_year)
+    return render_template("index.html",
+                           copyright_year=copyright_year,
+                           posts=posts)
 
 
 if __name__ == "__main__":
