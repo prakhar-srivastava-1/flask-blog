7a52530f6c75bd423155c8802ea38f8abf00b5b4
Prakhar Srivastava <prakhar.srivastava.2409@gmail.com>
2022-01-30 02:24:59 +0530

    add dynamic copyright

--- server.py
+++ server.py
@@ -1,11 +1,14 @@
-from flask import Flask
+import datetime as dt
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "Hello World!"
+    copyright_year = dt.datetime.now().strftime("%Y")
+    return render_template("index.html", copyright_year=copyright_year)
 
 
 if __name__ == "__main__":
