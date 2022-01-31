badcbb90cf9c42d9cc562304a247573fd0460885
Prakhar Srivastava <prakhar.srivastava.2409@gmail.com>
2022-01-31 07:00:40 +0530

    update footer.html - copyright information; pass dynamic copyright_year

--- server.py
+++ server.py
@@ -3,6 +3,8 @@ import datetime as dt
 import requests
 from flask import Flask, render_template
 
+COPYRIGHT_YEAR = dt.datetime.now().strftime("%Y")
+
 app = Flask(__name__)
 
 
@@ -12,20 +14,22 @@ def index():
     url = "https://api.npoint.io/c790b4d5cab58020d391"
     response = requests.request(url=url, method='GET')
     posts = response.json()
-    copyright_year = dt.datetime.now().strftime("%Y")
+
     return render_template("index.html",
-                           copyright_year=copyright_year,
+                           copyright_year=COPYRIGHT_YEAR,
                            posts=posts)
 
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html",
+                           copyright_year=COPYRIGHT_YEAR)
 
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html",
+                           copyright_year=COPYRIGHT_YEAR)
 
 
 @app.route("/blog/<int:post_id>")
@@ -41,9 +45,8 @@ def get_blog(post_id):
             select_post = post
             break
 
-    copyright_year = dt.datetime.now().strftime("%Y")
     return render_template("post.html",
-                           copyright_year=copyright_year,
+                           copyright_year=COPYRIGHT_YEAR,
                            post=select_post)
 
 
